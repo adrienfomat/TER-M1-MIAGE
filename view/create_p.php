34b0bdbb853ecc733170a3a5e516b44ae367ac82
Hamza-Riabi <Riabi.hamza01@gmail.com>
2025-02-08 20:14:54 +0100

    stylecreatepost and create_p

---
+++ view/create_p.php
@@ -0,0 +1,77 @@
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="stylecreatepost.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Accueil</title>
+    
+</head>
+<body>
+    
+    
+
+    
+    <div class="left-bar"></div>
+    
+
+    
+        <div class="main-box">
+            <div class="box">
+                <div class="titre">
+                    <h1>titre</h1>
+                    <p><b>27/03/2002</b></p>
+                </div>
+                <div class="contenu">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+   
+                </div>
+                <div class="sharedwith"><p>shared with :   </p>
+                    <input type="text" placeholder="Rechercher..." name="search"> </div>
+                <div class="people">    
+                <label for="sharedwith">Fallou
+                    <span class="delete-icon">&times;</span>
+                </label>
+                <label for="sharedwith">Hamza
+                    <span class="delete-icon">&times;</span>
+                </label>
+                <label for="sharedwith">Soumaya
+                    <span class="delete-icon">&times;</span>
+                </label></div>
+
+                <div class="bottompostit">
+                    <button class="save-button">SAVE</button>
+    <div class="color-palette">
+        <div class="color" style="background-color: red;" onclick="selectColor(event)"></div>
+        <div class="color" style="background-color: lightcoral;" onclick="selectColor(event)"></div>
+        <div class="color" style="background-color: lightgreen;" onclick="selectColor(event)"></div>
+        <div class="color" style="background-color: brown;" onclick="selectColor(event)"></div>
+        <div class="color" style="background-color: yellow;" onclick="selectColor(event)"></div>
+        <div class="color" style="background-color: darkblue;" onclick="selectColor(event)"></div>
+        <div class="color" style="background-color: lightblue;" onclick="selectColor(event)"></div>
+    </div>
+            </div>
+
+            </div>
+
+            <div class="overlay">
+                
+                
+            </div>
+            
+        </div>
+        <div class="right-bar"></div>
+        
+    
+    
+
+<script src="script.js"></script>
+</body>
+</html>
